a4ae440ea6100cf83ba63eeda77032039b93c44c
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-05-28 14:54:51 -0300

    usa cors nos serviços

--- projects/blog/posts/index.js
+++ projects/blog/posts/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Vamos salvar em memórias os nossos posts criados
 const posts = {};
